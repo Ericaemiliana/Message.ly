f5df9b5790629cbdba58fbd9bcc6e18b92d36838
erica pantoja <ericap@marsreturnship.com>
2022-02-09 17:03:02 -0500

    adding some changes

--- twilio-demo.js
+++ twilio-demo.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 var twilio = require("twilio");
+
 var client = new twilio(process.env.ACCOUNT_SID, process.env.AUTH_TOKEN);
 
 client.messages
@@ -8,4 +9,5 @@ client.messages
     to: "+14703380182", // your real-world cell number
     from: "+17753838706", // your Twilio phone number
   })
+  .catch((err) => console.log("ERR", err))
   .then((message) => console.log(message.sid));
